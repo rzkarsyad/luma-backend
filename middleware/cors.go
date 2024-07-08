3b473a68d75ee466e85c73e63704cb695ed3f103
Rizki Arsyad <arsyadrizki16@gmail.com>
2024-07-08 21:24:01 +0700

    change cors url origin

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://luma-app.vercel.app/")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://luma-app.vercel.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, jwt_token, session_id")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
