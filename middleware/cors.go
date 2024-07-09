cdaf53e19e32831415d2797f83f209d07cadd111
Rizki Arsyad <arsyadrizki16@gmail.com>
2024-07-09 07:47:12 +0700

    add subdomain in cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://luma.my.id")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://luma.my.id, https://www.luma.my.id, https://api.luma.my.id")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, jwt_token, session_id")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
